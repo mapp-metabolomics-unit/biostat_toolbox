4c5d027aebcde75209603aaf8c7ae82ba1afd308
PMA <pierre-marie.allard@unifr.ch>
2023-08-16 10:20:46 +0200

    up volcano

--- src/biostat_toolbox.r
+++ src/biostat_toolbox.r
@@ -2261,46 +2261,11 @@ for (condition in conditions) {
 
   ### Defining the plotly object 
 
-fig <- plot_ly()
-# Add traces
-fig <- fig %>% add_trace(x = ~1:3, y = ~10*(4:6), name = "yaxis data", mode = "lines+markers", type = "scatter")
-
-ay <- list(
-  tickfont = list(color = "red"),
-  overlaying = "y",
-  side = "right",
-  title = "<b>secondary</b> yaxis title")
-
-fig <- fig %>% add_trace(x = ~2:4, y = ~4:6, name = "yaxis 2 data", yaxis = "y2", mode = "lines+markers", type = "scatter")
-
-# Set figure title, x and y-axes titles
-fig <- fig %>% layout(
-  title = "Double Y Axis Example", yaxis2 = ay,
-  xaxis = list(title="xaxis title "),
-  yaxis = list(title="<b>primary</b> yaxis title")
-)%>%
-  layout(plot_bgcolor='#e5ecf6',
-          xaxis = list(
-            zerolinecolor = '#ffff',
-            zerolinewidth = 2,
-            gridcolor = 'ffff'),
-          yaxis = list(
-            zerolinecolor = '#ffff',
-            zerolinewidth = 2,
-            gridcolor = 'ffff')
-          )
-
-fig
-
-  ay <- list(
-  tickfont = list(color = "red"),
-  overlaying = "y",
-  side = "right",
-  title = "<b>secondary</b> yaxis title")
 
   plotly_volcano <- plot_ly(m, x = ~log2_fold_change, y = ~pvalue_minus_log10) %>%
     add_markers(text = row.names(m)) %>%
-    add_markers(text = row.names(m),yaxis='y2') %>%
+    add_markers(text = row.names(m),yaxis='y2'
+    ) %>%
     # config(displayModeBar = FALSE) %>%
     layout(
       title = "Hold shift while clicking \n markers for persistent selection",
@@ -2323,6 +2288,7 @@ fig
   )  %>% 
   layout(showlegend = FALSE)
 
+  # gg_plotly_volcano <- ggplotly(plotly_volcano)
 
   ### Defining the DT object
 
@@ -2390,11 +2356,19 @@ fig
     DT_volcano
   )
 
+  plotly_DT_crosstalked_div <- browsable(div(
+    div(style="display: grid; grid-template-columns: 1fr;", 
+        plotly_volcano %>%
+      highlight(
+        color = "green", on = "plotly_selected",
+        off = "plotly_deselect"
+      )),DT_volcano))
+
   ### Saving the plotly_DT_crosstalked object
   
   if (params$operating_system$system == "unix") {
     ###linux version
-    htmltools::save_html(plotly_DT_crosstalked, file = paste0("Volcano_DT_", first_part, "_vs_", second_part, ".html"))
+    htmltools::save_html(plotly_DT_crosstalked_div, file = paste0("Volcano_DT_", first_part, "_vs_", second_part, ".html"))
   }
 
 
